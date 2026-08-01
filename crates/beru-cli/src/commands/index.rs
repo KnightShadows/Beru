5cd17ab389a96a81cbff22e5d68a0652335e6e9b
adityaanand-tech <aditya19study@gmail.com>
2026-08-01 23:12:39 +0530

    beru inndex update

--- crates/beru-cli/src/commands/index.rs
+++ crates/beru-cli/src/commands/index.rs
@@ -28,7 +28,7 @@ pub fn exec(args: IndexArgs) -> Result<()> {
 
             let index_dir = cache.index_dir();
 
-            let default_url = "https://github.com/KnightShadows/Beru-index.git";
+            let default_url = "https://github.com/KnightShadows/beru-index.git";
             let url = url.as_deref().unwrap_or(default_url);
 
             info!("Updating Beru index from {}", url);
